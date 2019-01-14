3ce5b15218fe251659bc7e04238491ef6e0e7cca
eserna27 <eserna27@gmail.com>
2019-01-14 00:36:48 -0600

    parte rutas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get("/", function (){
+  return "Hola";
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/usuarios", function(){
+  return "usarios";
+});
+
+Route::get("/usuarios/nuevo", function(){
+  return "nuevo";
+});
+
+Route::get("/usuarios/{id}", function($id){
+  return "Mostrando detalle del usuario: {$id}";
+});
+
+Route::get("/saludo/{name}/{last_name}", function($name, $last_name){
+  return "Hola {$name} {$last_name}";
 });
